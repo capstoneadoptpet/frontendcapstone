52d435f629d7664b3ed1225197a57bb2d4f37c8c
Agung <agungmaulana7703@gmail.com>
2025-05-31 22:32:54 +0700

    Membetulkan card post

--- src/pages/dasboard/dasboard.jsx
+++ src/pages/dasboard/dasboard.jsx
@@ -1,6 +1,28 @@
+import { useState, useEffect } from "react";
+
 const Dasboard = () => {
+    const [totalPosts, setTotalPosts] = useState(0);
+    const apiURL = import.meta.env.VITE_API_URL;
+
+    useEffect(() => {
+        const fetchPets = async () => {
+            try {
+                const response = await fetch(`${apiURL}/pets`);
+                if (!response.ok) {
+                    throw new Error("Failed to fetch pets");
+                }
+                const datas = await response.json();
+                setTotalPosts(datas.data.length);
+                
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchPets();
+    }, [apiURL]);
+
     return (
-    
         <div className="ml-20 h-screen">
             <div className="mx-auto md:mx-[3rem] p-4">
                 <div className="justify-items-center border-b-2 my-[2rem]">
@@ -9,7 +31,7 @@ const Dasboard = () => {
                 <div className="grid grid-cols-2 gap-1 justify-items-center">
                     <div className="w-[18rem] h-[9rem] grid grid-rows-2 justify-items-center-safe rounded-2xl border-2 border-[var(--black)] bg-[var(--white)] ">
                         <h1 className="text-xl font-semibold p-2 m2">Jumlah Postingan</h1>
-                        <p className="text-2xl font-semibold text-[var(--blue-sky)]">12</p>
+                        <p className="text-2xl font-semibold text-[var(--blue-sky)]">{totalPosts}</p>
                     </div>
                     <div className="w-[18rem] h-[9rem] grid grid-rows-2 justify-items-center-safe rounded-2xl border-2 border-[var(--black)] bg-[var(--white)] ">
                         <h1 className="text-xl font-semibold p-2 m2">User</h1>
@@ -17,7 +39,6 @@ const Dasboard = () => {
                     </div>
                 </div>
             </div>
-            
         </div>
     )
 }
