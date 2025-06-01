fdec322ca7393bdf708db656b45891f497f88ee0
Agung <agungmaulana7703@gmail.com>
2025-06-01 16:42:02 +0700

    fix navbar

--- src/components/navigation-components/navbar.jsx
+++ src/components/navigation-components/navbar.jsx
@@ -35,7 +35,6 @@ const Navbar = () => {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-
   if (user && user.is_admin){
     return (
       <>      
@@ -49,173 +48,171 @@ const Navbar = () => {
               </span>
             </Link>
           </div>
-            <div className="flex space-x-4">
-              <span className="text-2xl">Hi, Adopt House Admin!</span>
-            </div>
-            </ul>
-          </nav>
-          {user && user.is_admin && (
-        <Mobile user={user} onClose={() => setShowSidebar(false)} onOpen={() => setShowSidebar(true)}  isOpen={showSidebar} />      
+          <div className="flex space-x-4">
+            <span className="text-2xl">Hi, Adopt House Admin!</span>
+          </div>
+        </ul>
+      </nav>
+      {user && user.is_admin && (
+        <Mobile_Sidebar user={user} onClose={() => setShowSidebar(false)} onOpen={() => setShowSidebar(true)}  isOpen={showSidebar} />      
       )}
       </>
     )
-  }
-
-  else {
-  if (isMobile) {
-    return (
-      <>
-        <nav className="bg-(--navy)  text-(--white)">
-          {token ? (
-          <ul className="flex justify-between items-center p-(--header-height) lg:flex-row">
-            <div className="flex items-center">
-              <Link to="/">
-                <span className='text-2xl font-extrabold'>
-                  <img src={logo} alt="Logo" className="h-24 w-24 inline-block mr-2" />
-                  Adopt House
-                </span>
-              </Link>
-            </div>
+  } else {
+    if (isMobile) {
+      return (
+        <>
+          <nav className="bg-(--navy)  text-(--white)">
             {token ? (
-              <div className="flex space-x-12">
-                <li>
-                  <button
-                    type="button"
-                    className={`flex items-center text-2xl cursor-pointer underline-offset-10 ${location.pathname === '/profile' ? 'underline' : ''}`}
-                    onClick={() => setShowSidebar(true)}
-                  >
-                    {user && user.picture && (
-                      <img
-                        src={`${apiURL}/${user.picture}`}
-                        alt="Profile"
-                        className="w-8 h-8 rounded-full mr-2 border object-cover"
-                      />
-                    )}
-                    Profile
-                  </button>
-                </li>
-                <li className="flex items-center">
-                  <button
-                    type="button"
-                    className='flex items-center'
-                    onClick={() => setShowMobileSidebar(true)}
-                  >
-                      <FaBars className="text-3xl"/>
-                  </button>
-                </li>
-              </div>
-              </ul>
-            ) : (
-          <ul className="flex justify-between items-center p-(--header-height) lg:flex-row">
-            <div className="flex items-center">
-              <Link>
-                <span className='text-2xl font-extrabold'>
-                  <img src={logo} alt="Logo" className="h-24 w-24 inline-block mr-2" />
-                  Adopt House
-                </span>
-              </Link>
-            </div>
-              <div className="flex space-x-4">
-                <li>
-                  <Link
-                    to="/register"
-                    className={`text-2xl hover:underline underline-offset-10 ${location.pathname === '/register' ? 'underline' : ''}`}>
-                    Sign Up
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/login"
-                    className={`text-2xl hover:underline underline-offset-10 ${location.pathname === '/login' ? 'underline' : ''}`}>
-                    Sign In
-                  </Link>
-                </li>
-              </div>
-              </ul>
-            )}
-        </nav>
-        {user && showSidebar && (
-              <Sidebar user={user} onClose={() => setShowSidebar(false)} isOpen={true} />
-            )}
-
-        {user && showMobileSidebar && (
-          <Mobile_Sidebar user={user} onClose={() => setShowMobileSidebar(false)} isOpen={showMobileSidebar} />      
-
-        )}
-      </>
-    )} else{
-        return (
-          <>
-            <nav className="bg-(--navy)  text-(--white)">
               <ul className="flex justify-between items-center p-(--header-height) lg:flex-row">
                 <div className="flex items-center">
-                  <Link to="/" className="hover:underline">
+                  <Link to="/">
                     <span className='text-2xl font-extrabold'>
                       <img src={logo} alt="Logo" className="h-24 w-24 inline-block mr-2" />
                       Adopt House
                     </span>
                   </Link>
                 </div>
-                {token ? (
-                  <div className="flex space-x-4">
-                    <li>
-                      <Link
-                        to="/"
-                        className={`text-2xl underline-offset-10 ${location.pathname === '/' ? 'underline' : ''}`}>
-                        Beranda
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        to="/findpet"
-                        className={`text-2xl underline-offset-10 ${location.pathname === '/findpet' ? 'underline' : ''}`}>
-                        Temukan Hewan
-                      </Link>
-                    </li>
-                    <li>
-                      <button
-                        type="button"
-                        className={`flex items-center text-2xl cursor-pointer underline-offset-10 ${location.pathname === '/profile' ? 'underline' : ''}`}
-                        onClick={() => setShowSidebar(true)}
-                      >
-                        {user && user.picture && (
-                          <img
-                            src={`${apiURL}/${user.picture}`}
-                            alt="Profile"
-                            className="w-8 h-8 rounded-full mr-2 border object-cover"
-                          />
-                        )}
-                        Profile
-                      </button>
-                    </li>
+                <div className="flex space-x-12">
+                  <li>
+                    <button
+                      type="button"
+                      className={`flex items-center text-2xl cursor-pointer underline-offset-10 ${location.pathname === '/profile' ? 'underline' : ''}`}
+                      onClick={() => setShowSidebar(true)}
+                    >
+                      {user && user.picture && (
+                        <img
+                          src={`${apiURL}/${user.picture}`}
+                          alt="Profile"
+                          className="w-8 h-8 rounded-full mr-2 border object-cover"
+                        />
+                      )}
+                      Profile
+                    </button>
+                  </li>
+                  <li className="flex items-center">
+                    <button
+                      type="button"
+                      className='flex items-center'
+                      onClick={() => setShowMobileSidebar(true)}
+                    >
+                      <FaBars className="text-3xl"/>
+                    </button>
+                  </li>
+                </div>
+              </ul>
+            ) : (
+              <>
+                <ul className="flex justify-between items-center p-(--header-height) lg:flex-row">
+                  <div className="flex items-center">
+                    <Link>
+                      <span className='text-2xl font-extrabold'>
+                        <img src={logo} alt="Logo" className="h-24 w-24 inline-block mr-2" />
+                        Adopt House
+                      </span>
+                    </Link>
                   </div>
-                ) : (
                   <div className="flex space-x-4">
                     <li>
                       <Link
                         to="/register"
-                        className={`text-2xl underline-offset-10 ${location.pathname === '/register' ? 'underline' : ''}`}>
+                        className={`text-2xl hover:underline underline-offset-10 ${location.pathname === '/register' ? 'underline' : ''}`}>
                         Sign Up
                       </Link>
                     </li>
                     <li>
                       <Link
                         to="/login"
-                        className={`text-2xl underline-offset-10 ${location.pathname === '/login' ? 'underline' : ''}`}>
+                        className={`text-2xl hover:underline underline-offset-10 ${location.pathname === '/login' ? 'underline' : ''}`}>
                         Sign In
                       </Link>
                     </li>
                   </div>
-                )}
-              </ul>
-            </nav>
-            {user && showSidebar && (
-              <Sidebar user={user} onClose={() => setShowSidebar(false)} isOpen={true} />
+                </ul>
+              </>
             )}
-          </>
-        );
+          </nav>
+          {user && showSidebar && (
+            <Sidebar user={user} onClose={() => setShowSidebar(false)} isOpen={true} />
+          )}
+          {user && showMobileSidebar && (
+            <Mobile_Sidebar user={user} onClose={() => setShowMobileSidebar(false)} isOpen={showMobileSidebar} />      
+          )}
+        </>
+      )
+    } else {
+      return (
+        <>
+          <nav className="bg-(--navy)  text-(--white)">
+            <ul className="flex justify-between items-center p-(--header-height) lg:flex-row">
+              <div className="flex items-center">
+                <Link to="/" className="hover:underline">
+                  <span className='text-2xl font-extrabold'>
+                    <img src={logo} alt="Logo" className="h-24 w-24 inline-block mr-2" />
+                    Adopt House
+                  </span>
+                </Link>
+              </div>
+              {token ? (
+                <div className="flex space-x-4">
+                  <li>
+                    <Link
+                      to="/"
+                      className={`text-2xl underline-offset-10 ${location.pathname === '/' ? 'underline' : ''}`}>
+                      Beranda
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/findpet"
+                      className={`text-2xl underline-offset-10 ${location.pathname === '/findpet' ? 'underline' : ''}`}>
+                      Temukan Hewan
+                    </Link>
+                  </li>
+                  <li>
+                    <button
+                      type="button"
+                      className={`flex items-center text-2xl cursor-pointer underline-offset-10 ${location.pathname === '/profile' ? 'underline' : ''}`}
+                      onClick={() => setShowSidebar(true)}
+                    >
+                      {user && user.picture && (
+                        <img
+                          src={`${apiURL}/${user.picture}`}
+                          alt="Profile"
+                          className="w-8 h-8 rounded-full mr-2 border object-cover"
+                        />
+                      )}
+                      Profile
+                    </button>
+                  </li>
+                </div>
+              ) : (
+                <div className="flex space-x-4">
+                  <li>
+                    <Link
+                      to="/register"
+                      className={`text-2xl underline-offset-10 ${location.pathname === '/register' ? 'underline' : ''}`}>
+                      Sign Up
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/login"
+                      className={`text-2xl underline-offset-10 ${location.pathname === '/login' ? 'underline' : ''}`}>
+                      Sign In
+                    </Link>
+                  </li>
+                </div>
+              )}
+            </ul>
+          </nav>
+          {user && showSidebar && (
+            <Sidebar user={user} onClose={() => setShowSidebar(false)} isOpen={true} />
+          )}
+        </>
+      );
     }
-};
+  }
 };
 
 export default Navbar;
